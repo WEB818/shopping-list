135040659b5f871c48939e58205ca53e86d8fc2c
Wendy Bartos <wendyebartos@gmail.com>
2019-10-24 19:50:00 -0700

    functional shopping list

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-//in the input form, users enter text and hit return or add. when they submit, event.currentTarget (submit button) adds the text to the list
-//toggle back and forth with check button 
-//delete with delete button
 'use strict';
 $(function () {
+
   $('#js-shopping-list-form').submit(event => {
     event.preventDefault();
 
     const newItemToBuy = $('#shopping-list-entry').val();
-    console.log(newItemToBuy);
 
     $('ul').append(`<li>
     <span class="shopping-item">${newItemToBuy}</span>
@@ -21,29 +18,18 @@ $(function () {
     </div>
     </li>`);
   });
-
     
-
-  $('.shopping-item-delete').click(event => {
-    event.preventDefault()
-    console.log('delete button clicked');
-        
-    const deleter = $(event.currentTarget).closest('li').attr('id','delete-me');
+  $('.shopping-list').on('click', '.shopping-item-delete', event => {
+    
+    $(event.currentTarget).closest('li').attr('id','delete-me');
        
-    const itemNotNeeded = $('#delete-me').remove();
+    $('#delete-me').remove();
   });
-
      
-        
-
-  $('.shopping-item').on('click', '.shopping-item-toggle', event => {
-    event.preventDefault();
-    console.log('check button clicked');
-            
-    const checkOff = $(event.currentTarget).closest('li').attr('class','check-me');//.toggleClass('shopping-item__checked');
-    const checkComplete = $('.check-me').toggleClass('shopping-item__checked');
+  $('.shopping-list').on('click', '.shopping-item-toggle', event => {
+         
+    $(event.currentTarget).closest('li').toggleClass('shopping-item__checked');
   });
-
 });
         
     
